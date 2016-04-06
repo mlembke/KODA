2512836fae118d8f58a293dd8727f2e994c19103
Michał Dobrzański <scoobiedoobielbn@gmail.com>
2016-04-06 21:26:23 +0200

    Predictive encoding: top and left neighbour

---
+++ PredictiveCodec.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+import numpy as np
+import itertools
+
+from PIL import Image
+
+def encode_predictive(image,opt):
+    width = image.size[0]
+    height = image.size[1]
+    pix = np.array(image,dtype='int64')
+    pix_diff = np.copy(pix)
+    for j in range(0, height):
+        for i in range(0, width):
+            # lewy sasiad
+            if (opt == 1) and (j > 0):
+                pix_diff[i,j] = pix[i, j] - pix[i, j - 1]
+            # gorny sasiad
+            if (opt == 2) and (i > 0):
+                pix_diff[i,j] = pix[i, j] - pix[i - 1, j]
+            # medianowe
+            # TODO
+    return pix_diff.flatten()
--- main.py
+++ main.py
@@ -2,11 +2,13 @@ import numpy as np
 
 import HuffmanCodec
 import ImageGenerator
-
+import PredictiveCodec
 
 def main():
-    image = ImageGenerator.generate_gaussian('out.bmp')
-    data = np.array(image).flatten()
+    image = ImageGenerator.generate_uniform('out.bmp')
+    data = PredictiveCodec.encode_predictive(image,1)
+    #data = np.array(image).flatten()
+    #print(data)
     code_book = HuffmanCodec.generate_huffman_code_book(data)
     code = HuffmanCodec.encode(data, code_book)
     with open('out.bin', 'wb') as f:
