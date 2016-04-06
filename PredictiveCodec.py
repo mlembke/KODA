9397d5ec05d96f314ea3673593952e726493db65
Magnus-S <mjkrajsman@gmail.com>
2016-04-06 22:59:27 +0200

    Predictive encoding: median + cosmetic changes in main.py and PredictiveCodec.py

--- PredictiveCodec.py
+++ PredictiveCodec.py
@@ -4,19 +4,21 @@ import itertools
 
 from PIL import Image
 
-def encode_predictive(image,opt):
+def encode_predictive(image, opt):
     width = image.size[0]
     height = image.size[1]
-    pix = np.array(image,dtype='int64')
+    pix = np.array(image, dtype='int64')
     pix_diff = np.copy(pix)
     for j in range(0, height):
         for i in range(0, width):
             # lewy sasiad
             if (opt == 1) and (j > 0):
-                pix_diff[i,j] = pix[i, j] - pix[i, j - 1]
+                pix_diff[i, j] = pix[i, j] - pix[i, j - 1]
             # gorny sasiad
             if (opt == 2) and (i > 0):
-                pix_diff[i,j] = pix[i, j] - pix[i - 1, j]
+                pix_diff[i, j] = pix[i, j] - pix[i - 1, j]
             # medianowe
-            # TODO
+            if (opt == 3) and (i > 0) and (j > 0):
+                pix_diff[i, j] = np.median([(pix[i, j] - pix[i - 1, j]), (pix[i, j] - pix[i, j - 1]),
+                                            (pix[i, j] - pix[i - 1, j - 1])])
     return pix_diff.flatten()
--- main.py
+++ main.py
@@ -4,11 +4,12 @@ import HuffmanCodec
 import ImageGenerator
 import PredictiveCodec
 
+
 def main():
     image = ImageGenerator.generate_uniform('out.bmp')
-    data = PredictiveCodec.encode_predictive(image,1)
-    #data = np.array(image).flatten()
-    #print(data)
+    data = PredictiveCodec.encode_predictive(image, 1)
+    # data = np.array(image).flatten()
+    # print(data)
     code_book = HuffmanCodec.generate_huffman_code_book(data)
     code = HuffmanCodec.encode(data, code_book)
     with open('out.bin', 'wb') as f:
