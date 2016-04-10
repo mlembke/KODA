5113052bb8752788afe0bbbef82b094a78674ab3
Magnus-S <mjkrajsman@gmail.com>
2016-04-10 17:15:13 +0200

    Tests upgrade (MedianTest.py)

--- MedianTest.py
+++ MedianTest.py
@@ -26,32 +26,57 @@ def mid(a, b, c):
 def count_time(a, b, c, opt):
     start_time = time()
     if opt == 0:
-        np.median([a, b, c])
+        result = np.median([a, b, c])
     else:
-        mid(a, b, c)
+        result = mid(a, b, c)
     action_time = time() - start_time
-    if opt == 0:
-        print('np.median time: ' + str(action_time))
-    else:
-        print('mid time: ' + str(action_time))
-    return action_time
+    # if opt == 0:
+    #     print('np.median time: ' + str(action_time))
+    # else:
+    #     print('mid time: ' + str(action_time))
+    return action_time, result
 
 
 def __test():
     mid_times = []
     median_times = []
-    amount = 150
-    for i in range(0, amount):
+    total_attempts = 512
+    mid_errs = 0
+    for i in range(0, total_attempts):
         a, b, c = random()*1000, random()*1000, random()*1000
-        mid_times.append(1000*count_time(a, b, c, 1))
-        median_times.append(1000*count_time(a, b, c, 0))
+        mid = 1000*count_time(a, b, c, 1)
+        median = 1000*count_time(a, b, c, 0)
+        mid_times.append(mid[0])
+        median_times.append(median[0])
+        print(str(mid[1]) + ' ' + str(median[1]))
+        if mid[1] != median[1]:
+            mid_errs+=1
 
-    plt.plot(range(0, amount), mid_times,'b+', linewidth=2.0, label='mid')
-    plt.plot(range(0, amount), median_times, 'rx', linewidth=2.0, label='np.median')
+    total_mid_time = sum(mid_times)
+    total_median_time = sum(median_times)
+    print('Total number of attempts: ' + str(total_attempts))
+    print('mid total time (ms): ' + str(total_mid_time))
+    print('np.median total time (ms): ' + str(total_median_time))
+    print('Mid error count: ' + str(mid_errs))
+    if mid_errs==0:
+        print('Mid correct!')
+    else:
+        print('Mid incorect!')
+
+    if total_mid_time < total_median_time:
+        print('Better performance: mid')
+    elif total_mid_time > total_median_time:
+        print('Better performance: np.median')
+    else:
+        print('Equal performance: mid, np.median')
+
+
+    plt.plot(range(0, total_attempts), mid_times,'b+', linewidth=2.0, label='mid')
+    plt.plot(range(0, total_attempts), median_times, 'rx', linewidth=2.0, label='np.median')
     plt.title('mid vs np.median')
     plt.xlabel('attempt nr')
     plt.ylabel('time (ms)')
-    plt.axis([0, amount, -0.5*max(median_times), 1.5*max(median_times)])
+    plt.axis([0, total_attempts, -0.5*max(median_times), 1.5*max(median_times)])
     plt.grid(True)
     plt.legend()
     plt.show()
