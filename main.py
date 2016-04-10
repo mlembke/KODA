4564c9c8d63d32399f9878c6489c7f8c18add144
Michał <dobrzanski.michal.daniel@gmail.com>
2016-04-10 14:54:49 +0200

    Histogram and Information function

--- main.py
+++ main.py
@@ -41,10 +41,43 @@ def read_images():
 def get_data(images,idx):
     pix = []
     pix.append(np.array(images[idx], dtype='int64').flatten())
-    for i in range(1,3):
+    for i in range(1,4):
         pix.append(PredictiveCodec.encode_predictive(images[idx],i))
     return pix  # Tablica: 0 - dane wejsciowe, 1 - lewy sasiad, 2 - gorny sasiad, 3 - mediana)
 
+
+def histogram(data,names,idx):
+    plt.suptitle('Histogramy danych roznicowych dla ' + names[idx])
+    plt.subplot(1, 3, 1)
+    plt.title('Lewy sasiad')
+    plt.hist(data[1], bins = 20, color='blue')
+    plt.subplot(1, 3, 2)
+    plt.title('Gorny sasiad')
+    plt.hist(data[2], bins = 20, color='red')
+    plt.subplot(1, 3, 3)
+    plt.title('Mediana')
+    plt.hist(data[3], bins = 20, color='green')
+    plt.show()
+
+
+def information(data,names,idx):
+    print(names[idx])
+
+    print("Entropia danych wejsciowych: " + str(entropy(data[0])))
+    code = Coder.encode(data[0])
+    print("Srednia dlugosc slowa kodowego: " + str(word_length(data[0],code)))
+
+    print("Entropia danych roznicowych (gorny sasiad): " + str(entropy(data[1])))
+    code = Coder.encode(data[1])
+    print("Srednia dlugosc slowa kodowego: " + str(word_length(data[1],code)))
+
+    print("Entropia danych roznicowych (lewy sasiad): " + str(entropy(data[2])))
+    code = Coder.encode(data[2])
+    print("Srednia dlugosc slowa kodowego: " + str(word_length(data[2],code)))
+
+    print("Entropia danych roznicowych (mediana): " + str(entropy(data[3])))
+    code = Coder.encode(data[3])
+    print("Srednia dlugosc slowa kodowego: " + str(word_length(data[3],code)))
 
 def main():
 
@@ -54,17 +87,8 @@ def main():
     idx = 0
     data = get_data(imgs,idx)
 
-    plt.suptitle('Histogramy danych roznicowych dla ' + names[idx])
-    plt.subplot(1, 3, 1)
-    plt.title('Lewy sasiad')
-    plt.hist(data[0], bins = 20, color='blue')
-    plt.subplot(1, 3, 2)
-    plt.title('Gorny sasiad')
-    plt.hist(data[1], bins = 20, color='red')
-    plt.subplot(1, 3, 2)
-    #plt.title('Mediana')
-    # TODO
-    #plt.show()
+    histogram(data,names,idx)
+    information(data,names,idx)
 
     '''
     # Ksiazka przelaskowskiego (str. 81) entropia = 2.176
@@ -78,14 +102,6 @@ def main():
     tcode = Coder.encode(test)
     print("Test2: " + str(word_length(test,tcode))) # = 2.22580645161
     '''
-
-    print("Entropia dla " + names[idx])
-    print("danych wejsciowych: " + str(entropy(data[0])))
-    print("danych roznicowych (gorny sasiad): " + str(entropy(data[1])))
-    print("danych roznicowych (lewy sasiad): " + str(entropy(data[2])))
-    code = Coder.encode(data[0])
-    print("Srednia dlugosc slowa kodowego: " + str(word_length(data[0],code)))
-
 
     '''
     image = ImageGenerator.generate_gaussian()
